0e9833937b993e690c05f9644818f4a936e3f5da
Ali Salamatian <alisalamatian1@gmail.com>
2022-01-15 15:40:57 -0800

    Adding css and the submit button to the Post

--- frontEnd/problem-bank/src/Routes/Post.js
+++ frontEnd/problem-bank/src/Routes/Post.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import "../index.css"
 
-export default function Post() {
+const Post = ()=>{
+    const [message, setMessage] = useState(null);
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const submitHandler = ()=>{
+        setMessage({
+            title,
+            message,
+        })
+    }
+    
     return (
         <div>
             at the post!
             <Link to = "/">Problem Bank</Link>
-            <form>
-                <input type= "text" placeholder='Enter the problem'/>
+            <form class= "container-input">
+                <input class= "post-title-input" type= "text" placeholder='Enter the title'/> <br />
+                <input class= "post-body-input" type= "text" placeholder='Enter the problem body'/> <br />
+                <button onClick={submitHandler}>
+                    submit
+                </button>
             </form>
         </div>
     )
 }
+
+export default Post;
+
+// onChange={setTitle()}
+// onChange={setBody()}
