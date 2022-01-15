5eb292d34b222c7b6e9050dfb44cfb159b39d35d
Gordon Cheung <gordonhwc1@gmail.com>
2022-01-15 13:09:13 -0800

    running server

---
+++ backEnd/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+// Connect database
+//connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+//Routing
+/*
+app.use('/api/auth', require('./routing/problem'));
+*/
+
+const port = process.env.PORT || 8000;
+
+const server = app.listen(port, () => console.log(`Server running on port ${port}`));
