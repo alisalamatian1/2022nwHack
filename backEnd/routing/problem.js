dae1f47c78eb67691d2b258c02c689f6384297fd
Gordon Cheung <gordonhwc1@gmail.com>
2022-01-15 20:11:33 -0800

    add updateLike route

--- backEnd/routing/problem.js
+++ backEnd/routing/problem.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {list_all, post} = require('../controller/problem');
+const {list_all, post, updateLike} = require('../controller/problem');
 
 router.route("/list/all").get(list_all);
 router.route("/post").post(post);
+router.route("/updateLike").put(updatLike);
 
 module.exports = router;
